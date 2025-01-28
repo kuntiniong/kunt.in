dff0fe1c282f24cfe93750123d7576d248deb813
kuntiniong <dev@kunt.in>
2025-01-29 06:22:23 +0800

    Add SVG animation in home

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -6,3 +6,18 @@ window.addEventListener("load", () => {
     ease: "expoScale.out",
   });
 });
+
+// Wait for the DOM to load
+document.addEventListener("DOMContentLoaded", () => {
+  // Select all paths and rectangles in the SVG
+  const elements = document.querySelectorAll(".kun-svg path, .kun-svg rect");
+
+  // Loop through each element
+  elements.forEach((el) => {
+    // Get the total length of the element
+    const length = el.getTotalLength();
+
+    // Set the length as a CSS variable for use in dasharray and dashoffset
+    el.style.setProperty("--path-length", length);
+  });
+});
